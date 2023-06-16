80b72e11c3372bc35b845ac5c006b7b515afec9e
timpel301 <tim.pel301@gmail.com>
2023-06-16 23:31:25 +0200

    initial implementation attempt

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+var bodyparser = require('body-parser');
+const dns = require('node:dns');
+const mongoose = require('mongoose');
+require('nanoid');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +21,48 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.json());
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+app.route('/api/shorturl').post((req,res) => {
+  const createShort = (done) => {
+    var hostname = req.body.toString();
+    var short = nanoid();
+    if (!dns.lookup(hostname, err)){
+      try{
+        let url = Url.findOne({original_url: hostname});
+        if (url) {
+          res.json(url)
+        } else{
+          url = new Url({
+          original_url: hostname,
+          short_url: short,
+        });
+        url.save();
+        res.json(url);
+        done();
+        }   
+      } catch (err) {console.error(err);}
+    } else res.json({error: 'invalid url'});
+  }
+});
+
+app.get('/api/shorturl/:short', (req,res) => {
+  if (Url.findOne({short_url: req.params.short})){
+    let url = Url.findOne({short_url: req.params.short});
+    res.redirect(url.original_url);
+  } else {res.json({error: 'invalid url'});}
+
 });
 
 app.listen(port, function() {
