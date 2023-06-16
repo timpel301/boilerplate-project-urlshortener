f9780433786547e45f9d766ef8545dc8b9e6a57f
timpel301 <tim.pel301@gmail.com>
2023-06-17 00:07:16 +0200

    fixed version hopefully

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 var bodyparser = require('body-parser');
 const dns = require('node:dns');
 const mongoose = require('mongoose');
-require('nanoid');
+const { nanoid } = require('nanoid');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,16 +14,16 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.use(bodyparser.urlencoded({extended:true}));
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -34,37 +34,57 @@ urlSchema = new mongoose.Schema({
 
 const Url = mongoose.model('Url', urlSchema);
 
-app.route('/api/shorturl').post((req,res) => {
-  const createShort = (done) => {
-    var hostname = req.body.toString();
-    var short = nanoid();
-    if (!dns.lookup(hostname, err)){
-      try{
-        let url = Url.findOne({original_url: hostname});
-        if (url) {
-          res.json(url)
-        } else{
-          url = new Url({
+const createShort = (req, res) => {
+  var hostname = req.body.url;
+  var short = nanoid();
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      res.json({ error: 'Invalid URL' });
+      return;
+    }
+    Url.findOne({ original_url: hostname }).exec((err, url) => {
+      if (err) {
+        console.error(err);
+        res.json({ error: 'URL not in database' });
+        return;
+      }
+      if (url) {
+        res.json(url);
+      } else {
+        url = new Url({
           original_url: hostname,
           short_url: short,
         });
-        url.save();
-        res.json(url);
-        done();
-        }   
-      } catch (err) {console.error(err);}
-    } else res.json({error: 'invalid url'});
-  }
+        url.save((err) => {
+          if (err) {
+            console.error(err);
+            res.json({ error: 'not saved' });
+            return;
+          }
+          res.json(url);
+        });
+      }
+    });
+  });
+};
+
+app.route('/api/shorturl').post(createShort);
+
+app.get('/api/shorturl/:short', (req, res) => {
+  Url.findOne({ short_url: req.params.short }).exec((err, url) => {
+    if (err) {
+      console.error(err);
+      res.json({ error: 'Could not get url' });
+      return;
+    }
+    if (url) {
+      res.redirect(url.original_url);
+    } else {
+      res.redirect('/');
+    }
+  });
 });
 
-app.get('/api/shorturl/:short', (req,res) => {
-  if (Url.findOne({short_url: req.params.short})){
-    let url = Url.findOne({short_url: req.params.short});
-    res.redirect(url.original_url);
-  } else {res.json({error: 'invalid url'});}
-
-});
-
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
